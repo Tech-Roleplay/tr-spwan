9b54e2beb4be800846c6c38e0de5e018b4ddc5d3
Timm TheBlokker <theblokker.dev@gmail.com>
2024-03-28 10:07:11 +0100

    adding chat messages

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,14 @@
 /// <reference types="@altv/types-server" />
 
 import * as alt from 'alt-server';
+import * as chat from '../../chat/server/index.js';
 
 alt.on('playerConnect', (player: alt.Player) => {
     player.model = 'a_m_y_tramp_01';
     player.spawn(0,0,80,2);
+
+    chat.broadcast(`*[${player.id}]Player ${player.name} has connected!`);
+    alt.emitClient(player, 'spawnPlayer');
+
+    alt.log(`*[${player.id}]Player ${player.name} has with DC ${player.discordID} connected!`);
 });
