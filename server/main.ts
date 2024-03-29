233c9c89e5f7f9d57cb12334f48eb54b1d0d7582
Timm TheBlokker <theblokker.dev@gmail.com>
2024-03-29 16:48:49 +0100

    adding resource.toml

--- server/main.ts
+++ server/main.ts
@@ -4,11 +4,15 @@ import * as alt from 'alt-server';
 import * as chat from '../../chat/server/index.js';
 
 alt.on('playerConnect', (player: alt.Player) => {
+    alt.log(`*[${player.id}]Player ${player.name} has with DC ${player.discordID} connected!`);
+});
+
+alt.on('server:event:finish:authenticate', (player: alt.Player) => {
     player.model = 'a_m_y_tramp_01';
     player.spawn(0,0,80,2);
 
     chat.broadcast(`*[${player.id}]Player ${player.name} has connected!`);
     alt.emitClient(player, 'spawnPlayer');
 
-    alt.log(`*[${player.id}]Player ${player.name} has with DC ${player.discordID} connected!`);
-});
+    
+})
